7590e25b5a281a18f92c79eefe8210cabb526c97
Nityanshu Ranawat <64055499+nityanshu31@users.noreply.github.com>
2026-04-17 00:43:03 +0530

    Update hero.service.js

--- src/services/hero.service.js
+++ src/services/hero.service.js
@@ -1,12 +1,13 @@
 import { headers } from "next/headers";
 
 export async function getHeroData() {
-  const host = (await headers()).get("host");
+  const headersList = await headers();
+  const host = headersList.get("host");
+
+  if (!host) throw new Error("Host header missing");
 
   const protocol =
-    process.env.NODE_ENV === "development"
-      ? "http"
-      : "https";
+    process.env.NODE_ENV === "development" ? "http" : "https";
 
   const res = await fetch(`${protocol}://${host}/api/hero`, {
     cache: "no-store",
